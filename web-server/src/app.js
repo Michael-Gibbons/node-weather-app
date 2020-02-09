eec5510d9bff6ca014d650da5a33190c2625a433
Michael Gibbons <michael@btownweb.com>
2020-02-09 14:54:26 -0700

    Added header, footer partials and 404 page

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,61 @@
+const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
-app.get('', (req, res) => {
-  res.send("Hello Express");
-});
+// Define paths for express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
-app.get('/help', (req, res) => {
-  res.send("Help Page");
+// Setup handlebars, engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: "Weather App",
+    name: "Michael Gibbons"
+  });
 });
 
 app.get('/about', (req, res) => {
-  res.send("About Page");
+  res.render('about', {
+    title: "About Me",
+    name: "Michael Gibbons"
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    helpText: "This is a help message",
+    title: "Help",
+    name: "Michael Gibbons"
+  });
+});
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    errMsg: "HELP ARTICLE NOT FOUND",
+    title: "404",
+    name: "Michael Gibbons"
+  });
 });
 
 app.get('/weather', (req, res) => {
   res.send("View Weather");
+});
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    errMsg: "404 NOT FOUND",
+    title: "404",
+    name: "Michael Gibbons"
+  });
 });
 
 app.listen(3000, () => {
