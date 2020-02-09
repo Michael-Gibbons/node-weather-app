8634a1d5b77da45ae1c9418c641d5e564a2c624b
Michael Gibbons <michael@btownweb.com>
2020-02-09 11:47:38 -0700

    Added express server

--- weather-app/utils.js/forcast.js
+++ weather-app/utils.js/forcast.js
@@ -2,15 +2,15 @@ const request = require("request");
 
 const forcast = (latitude, longitude, callback) => {
   const url = "https://api.darksky.net/forecast/d29c484faf7760bd59394d7f937f57ac/" + latitude +"," + longitude;
-  request({ url: url, json: true }, (err, response) => {
+  request({ url: url, json: true }, (err, { body }) => {
     if(err){
       callback("Unable to connect to weather service!", undefined);
-    }else if(response.body.error){
+    }else if(body.error){
       callback("Unable to find location", undefined);
     }else{
       callback(undefined, {
-        currently: response.body.currently,
-        weatherString: response.body.daily.data[0].summary + " It is currently " + response.body.currently.temperature + " Degrees out. There is a " + response.body.currently.precipProbability + "% chance of rain"
+        currently: body.currently,
+        weatherString: body.daily.data[0].summary + " It is currently " + body.currently.temperature + " Degrees out. There is a " + body.currently.precipProbability + "% chance of rain"
       });
     }
   });
--- weather-app/utils.js/geocode.js
+++ weather-app/utils.js/geocode.js
@@ -2,16 +2,16 @@ const request = require("request");
 
 const geocode = (address, callback) => {
   const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + encodeURIComponent(address) + ".json?access_token=pk.eyJ1IjoiY2F2ZW1hbnh4MTMweCIsImEiOiJjazZlZHdpNHMwYXFxM25wNjh3MDlqbmk5In0.6r3PhVd1bEMX28h7Uj25xA&limit=1";
-  request({ url: url, json: true }, (err, response) => {
+  request({ url: url, json: true }, (err, { body }) => {
     if(err){
       callback("Unable to connect to location service!", undefined);
-    }else if(response.body.features.length === 0){
+    }else if(body.features.length === 0){
       callback("Unable to find location. Try another search.", undefined);
     }else{
       callback(undefined, {
-        latitude: response.body.features[0].center[1],
-        longitude: response.body.features[0].center[0],
-        location: response.body.features[0].place_name
+        latitude: body.features[0].center[1],
+        longitude: body.features[0].center[0],
+        location: body.features[0].place_name
       });
     }
   });
